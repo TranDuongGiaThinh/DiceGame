82fb2b631439f1264475b51524774254c3517865
Trần Dương Gia Thịnh <146810058+TranDuongGiaThinh@users.noreply.github.com>
2023-11-30 10:12:50 +0700

    add provider for ManageState

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:app/views/index_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => BowlState(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
